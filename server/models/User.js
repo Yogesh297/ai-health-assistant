150751aeff43de4c5523af8d9ce06dd030778219
Yogesh297 <157105872+Yogesh297@users.noreply.github.com>
2025-07-22 14:37:10 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -28,7 +28,8 @@ const userSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  isVerified: { type: Boolean, default: false }
 })
 
 // Hash password before saving
